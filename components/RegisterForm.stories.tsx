378ff9d0a95dc5624da34e7a76d515ccb439816c
motoyasu-hasegawa <hiroya.maekawa@fox-hound.jp>
2025-05-23 19:06:10 +0900

    maekawa
    task1-1 途中

---
+++ components/RegisterForm.stories.tsx
@@ -0,0 +1,18 @@
+// components/RegisterForm.stories.tsx
+// import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import RegisterForm from "./RegisterForm";
+
+const meta: Meta<typeof RegisterForm> = {
+  title: "Components/RregisterForm",
+  component: RegisterForm,
+};
+
+export default meta;
+type Story = StoryObj<typeof RegisterForm>;
+
+export const Default: Story = {};
+
+// TODO メタデータ
+// TODO ストーリーの
+// TODO デフォルトストーリーの
